92865f91aa4b8f3145b3e044596e98242ee2e7e2
Juliorocktect <113830349+Juliorocktect@users.noreply.github.com>
2023-03-20 16:08:48 +0100

    Initial commit

---
+++ src/test/java/com/stream/Site/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.Site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
