7908b0a42589bb4ea96e9ac9d8cca4c66f8c762e
juli <julius.k@upsala>
2023-03-29 20:33:48 +0200

    Mongo DB integration

--- src/main/java/com/stream/Site/Repository/UserRepo.java
+++ src/main/java/com/stream/Site/Repository/UserRepo.java
@@ -2,8 +2,6 @@ package com.stream.Site.Repository;
 
 import com.stream.Site.Model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-@Repository
 public interface UserRepo extends MongoRepository<User, String> {
 
 }
--- src/main/java/com/stream/Site/Repository/VideoRepo.java
+++ src/main/java/com/stream/Site/Repository/VideoRepo.java
@@ -2,8 +2,6 @@ package com.stream.Site.Repository;
 
 import com.stream.Site.Model.Video;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface VideoRepo extends MongoRepository<Video,String> {
 }
